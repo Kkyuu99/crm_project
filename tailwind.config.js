2726e0bcb334c792043285869f941442e3b9f020
Joseph <merrypar731@gmail.com>
2025-01-17 10:31:37 +0630

    hi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-    ],
+    content: ["./resources/**/*.blade.php",
+         "./resources/**/*.js",
+         "./resources/**/*.vue"],
     theme: {
       extend: {
         colors: {
