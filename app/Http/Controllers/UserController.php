ddd64f43a9c980f56355509fd3638bb012101fe1
Joseph <merrypar731@gmail.com>
2025-01-21 15:44:48 +0630

    crud 1st

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,29 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('users.index');
+        return view('users.index',[
+            'users' => User::latest()->get()
+        ]);
     }
 
-    public function create(){
+    public function create(Request $request){
+        $user = new User();
+        $user->name = $request->name;   
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->role = $request->role;
+        $user->project_id = $request->project_id;
+        $user->created_by = $request->created_by;
+        $user->updated_by = $request->updated_by;
+        $user->deleted_by = $request->deleted_by;
+        $user->save();
+
+
+        $validatedData = validator($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+            'project_id' => 'required',
+            'created_by' => 'required',
+            'updated_by' => 'required',
+            'deleted_by' => 'required',
+        ]);
+
         return view('users.create');
     }
 
-    public function show(){
-        return view('users.show');
+    public function show($id){
+        return view('users.show',
+        [
+            'user' => User::findOrFail($id)
+        ]);
     }
 
-    public function edit(){
+    public function edit(Request $request){
+        $user = new User();
+        $user->name = $request->name;   
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->role = $request->role;
+        $user->project_id = $request->project_id;
+        $user->created_by = $request->created_by;
+        $user->updated_by = $request->updated_by;
+        $user->deleted_by = $request->deleted_by;
+        $user->save();
+
+
+        $validatedData = validator($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+            'project_id' => 'required',
+            'created_by' => 'required',
+            'updated_by' => 'required',
+            'deleted_by' => 'required',
+        ]);
+
         return view('users.edit');
     }
 
-    public function delete(){
-        return view('users.delete');
+    public function delete($id){
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect('/')->with('success', 'User deleted successfully');  
     }
     
 }
