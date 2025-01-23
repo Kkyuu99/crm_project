91fa057ea67f91f585761d2a17438fbd6ccfcc45
kiki <meihua@gmail.com>
2025-01-23 22:17:54 +0630

    Project-db

---
+++ database/migrations/2025_01_22_035851_create_project_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->id();
+            $table->string('project_name');
+            $table->string('organization_name');
+            $table->string('project_type');
+            $table->string('project_manager');
+            $table->foreignId('issue_id');
+            $table->string('contact_name');
+            $table->string('contact_phone');
+            $table->string('contect_email');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->string('deleted_by');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project');
+    }
+};
