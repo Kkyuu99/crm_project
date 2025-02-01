7fa168f2d5cddcc645a2b31339d94bd85a6898fc
Joseph <merrypar731@gmail.com>
2025-02-01 23:15:48 +0630

    web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 Route::get('/', [UserController::class, 'index']);
 Route::get('/user/add', [UserController::class, 'create']);
 Route::post('/user/store', [UserController::class, 'store']);
@@ -12,8 +11,3 @@ Route::get('/user/show', [UserController::class, 'show']);
 Route::patch('/user/{user:name}/edit', [UserController::class, 'edit']);
 Route::delete('/user/{user:name}/delete', [UserController::class, 'delete']);
 
-=======
-Route::get('/', function () {
-    return view('admin.sidebar');
-});
->>>>>>> shoon
