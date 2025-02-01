181c875d107d54058671c4e7acd0bad08de08c0c
Kkyuu99 <akyuuisahmi7@gmail.com>
2025-02-01 19:42:04 +0630

    issuecontroller1

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -7,7 +7,91 @@ use app\Models\Issue;
 
 class IssueController extends Controller
 {
-    //
+    public function index(){
+        $issues = Issue::all();
+        //return view('issues.index', compact('issues'));
+    }
+
+    public function create(){
+        return view('issues.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'project_id' =>'required',
+            'subject' => 'required|string|max:255',
+            'description' => 'required',
+            'status' => 'required|string|max:255',
+            'priority' => 'required|string|max:255',
+            'attachment' => 'nullable|string|max:255',
+            'assignor_user' => 'required|string|max:255',
+            'remark' => 'nullable',
+            'total_duration' => 'required|string|max:255',
+            'created_at' => 'required',
+            'updated_at' => 'required',
+        ]);
+
+        Issue::create([
+            'id' => $request->input('id'),
+            'project_id' => $request->input('project_id'),
+            'subject' => $request->input('subject'),
+            'description' => $request->input('description'),
+            'status' => $request->input('issue_status'),
+            'priority' => $request->input('priority'),
+            'attachment' => $request->input('attachment'),
+            'assignor' => $request->input('assignor_user'),
+            'remark' => $request->input('remark'),
+            'total_duration' => $request->input('total_duration'),
+            'created' => $request->created_at,
+            'updated' => $request->updated_at,
+        ]);
+    }
+
+    public function list(){
+        $issues = Issue::all();
+        //return view('issues.list',compact('issues'));
+    }
+
+    public function edit(Issue $issues){
+        return view('issues.edit',compact('issues'));
+    }
+
+    public function update(Request $request, Issue $issue)
+    {
+        $request->validate([
+            'project_id' =>'required',
+            'subject' => 'required|string|max:255',
+            'description' => 'required',
+            'status' => 'required|string|max:255',
+            'priority' => 'required|string|max:255',
+            'attachment' => 'nullable|string|max:255',
+            'assignor_user' => 'required|string|max:255',
+            'remark' => 'nullable',
+            'total_duration' => 'required|string|max:255',
+            'created_at' => 'required',
+            'updated_at' => 'required',
+        ]);
+
+        $issue->update([
+            'id' => $request->input('id'),
+            'project_id' => $request->input('project_id'),
+            'subject' => $request->input('subject'),
+            'description' => $request->input('description'),
+            'status' => $request->input('issue_status'),
+            'priority' => $request->input('priority'),
+            'attachment' => $request->input('attachment'),
+            'assignor' => $request->input('assignor_user'),
+            'remark' => $request->input('remark'),
+            'total_duration' => $request->input('total_duration'),
+            'created' => $request->created_at,
+            'updated' => $request->updated_at,
+        ]);
+    }
+
+    public function delete(Issue $issues){
+        $issues->delete();
+
+    }
 }
 ?>
-
--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Issue extends Model
 {
-    //
+    use HasFactory;
+    protected $guarded=['id'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
