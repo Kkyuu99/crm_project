6996fb8b182a8e30d094bb797a2516d697593cbc
kiki <meihua@gmail.com>
2025-02-07 11:38:11 +0630

    update crm

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
-use app\Models\Project;
 
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::all();
-        //return view('Projects.index', compact('Projects'));
+        $issues = Project::paginate(5);
+        //return view('Project.index', compact('Project'));
+    }
+
+    public function project_list(){
+        return view('user.project-list',[
+            'projects' => Project::paginate(5)
+        ]);
     }
 
     public function create(){
-        return view('Projects.create');
+        return view('Project.create');
     }
 
     public function store(Request $request)
@@ -21,12 +27,12 @@ class ProjectController extends Controller
         $request->validate([
             'issue_id' =>'required',
             'project_name' => 'required|string|max:255',
-            'organization_name' => 'required|string|max:255',
+            'organization_name' => 'required',
             'project_type' => 'required|string|max:255',
             'project_manager' => 'required|string|max:255',
-            'contact_name' => 'required|string|max:255',
+            'contact_name' => 'nullable|string|max:255',
             'contact_phone' => 'required|string|max:255',
-            'contact_email' => ['required|string|max:255',Rule::unique('users','email')],
+            'contact_email' => 'nullable|string|max:255',
             'created_by' => 'required|string|max:255',
             'updated_by' => 'required',
             'deleted_by' => 'required',
@@ -39,18 +45,20 @@ class ProjectController extends Controller
             'organization_name' => $request->input('organization_name'),
             'project_type' => $request->input('project_type'),
             'project_manager' => $request->input('project_manager'),
-            'contact_name' => $request->input('contact_name'),
-            'contact_phone' => $request->input('contact_phone'),
-            'contact_email' => [$request->input('contact_email'),Rule::unique('users','email')],
-            'created_by' => $request->created_by,
+            'contact_name' => 'nullable|string|max:255',
+            'contact_phone' => $request->input('attachment'),
+            'contact_email' => 'nullable|string|max:255' ,
+            'created_by' => $request->input('created_by'),
             'updated_by' => $request->created_at,
             'deleted_by' => $request->updated_at,
         ]);
+
+        return redirect('/');
     }
 
     public function list(){
         $project = Project::all();
-        //return view('project-list',compact('projects'));
+        //return view('project.list',compact('projects'));
     }
 
     public function edit(Project $projects){
@@ -62,12 +70,11 @@ class ProjectController extends Controller
         $request->validate([
             'issue_id' =>'required',
             'project_name' => 'required|string|max:255',
-            'organization_name' => 'required|string|max:255',
+            'organization_name' => 'required',
             'project_type' => 'required|string|max:255',
             'project_manager' => 'required|string|max:255',
-            'contact_name' => 'required|string|max:255',
+            'contact_name' => 'nullable|string|max:255',
             'contact_phone' => 'required|string|max:255',
-            'contact_email'=>['required','email',Rule::unique('users','email')],
             'created_by' => 'required|string|max:255',
             'updated_by' => 'required',
             'deleted_by' => 'required',
@@ -82,16 +89,15 @@ class ProjectController extends Controller
             'project_manager' => $request->input('project_manager'),
             'contact_name' => $request->input('contact_name'),
             'contact_phone' => $request->input('assignor_user'),
-            'contact_email' => [$request->input('remark'),Rule::unique('users','email')],
-            'created_by' => $request->created_by,
-            'uptaded_by' => $request->created_at,
+            'contact_email' => $request->input('remark'),
+            'created_by' => $request->input('created_by'),
+            'updated_by' => $request->created_at,
             'deleted_by' => $request->updated_at,
         ]);
     }
 
-    public function delete(Project $projects){
-        $projects->delete();
-
+    public function delete(Project $project){
+        $project->delete();
+        return redirect('/user/project-list');
     }
 }
-?>
--- database/migrations/2025_01_22_035851_create_project_table.php
+++ database/migrations/2025_01_22_035851_create_project_table.php
@@ -18,13 +18,12 @@ return new class extends Migration
             $table->string('project_type');
             $table->string('project_manager');
             $table->foreignId('issue_id');
-            $table->foreignId('user_id');
             $table->string('contact_name');
             $table->string('contact_phone');
-            $table->string('contact_email')->unique();
-            $table->string('created_by');
-            $table->string('updated_by');
-            $table->string('deleted_by');
+            $table->string('contact_email');
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
             $table->timestamps();
         });
     }
