2b978cd15e25ca559a075ae4c6d4087925205414
kiki <meihua@gmail.com>
2025-02-19 11:10:30 +0630

    update controller/list/detail & new

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,97 +7,109 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+    //show the list of all project
     public function index(){
-        $issues = Project::paginate(5);
-        //return view('Project.index', compact('Project'));
+        $projects = Project::paginate(5);
+        return view('user.project-list', compact('projects'));
     }
 
+    //show project-list with sorting
     public function project_list(){
-        return view('user.project-list',[
-            'projects' => Project::paginate(5)
-        ]);
+        $projects = Project::orderBy('created_at', 'desc')->paginate(5);
+        return view('user.project-list',compact('projects'));
     }
 
-    public function create(){
-        return view('Project.create');
+    public function project_detail(){
+        return view('user.project_detail');
     }
 
-    public function store(Request $request)
-    {
-        $request->validate([
+    public function project_create(){
+        return view('user.new_project');
+    }
+
+    public function store(Request $request){
+        //validate the incoming request
+        $data = $request->validate([
             'issue_id' =>'required',
             'project_name' => 'required|string|max:255',
-            'organization_name' => 'required',
+            'organization_name' => 'required|max:255',
             'project_type' => 'required|string|max:255',
             'project_manager' => 'required|string|max:255',
             'contact_name' => 'nullable|string|max:255',
             'contact_phone' => 'required|string|max:255',
             'contact_email' => 'nullable|string|max:255',
-            'created_by' => 'required|string|max:255',
+            'created_by' => 'required',
             'updated_by' => 'required',
-            'deleted_by' => 'required',
+            'deleted_by' => 'nullable',
         ]);
+        $newProject = Project::create($data);
+    
 
-        Project::create([
-            'id' => $request->input('id'),
-            'issue_id' => $request->input('issue_id'),
-            'project_name' => $request->input('project_name'),
-            'organization_name' => $request->input('organization_name'),
-            'project_type' => $request->input('project_type'),
-            'project_manager' => $request->input('project_manager'),
-            'contact_name' => 'nullable|string|max:255',
-            'contact_phone' => $request->input('attachment'),
-            'contact_email' => 'nullable|string|max:255' ,
-            'created_by' => $request->input('created_by'),
-            'updated_by' => $request->created_at,
-            'deleted_by' => $request->updated_at,
-        ]);
+    //     //create the new project in db
+    //     $project=Project::create($valiade);
+    
+    //     Project::create([
+    //         'id' => $request->input('id'),
+    //         'issue_id' => $request->input('issue_id'),
+    //         'project_name' => $request->input('project_name'),
+    //         'organization_name' => $request->input('organization_name'),
+    //         'project_type' => $request->input('project_type'),
+    //         'project_manager' => $request->input('project_manager'),
+    //         'contact_name' => 'nullable|string|max:255',
+    //         'contact_phone' => $request->input('contact_phone'),
+    //         'contact_email' => 'nullable|string|max:255' ,
+    //         'created_by' => $request->created_at,
+    //         'updated_by' => $request->updated_at,
+    //         'deleted_by' => $request->deleteed_at,
+    //     ]);
+        
 
-        return redirect('/');
-    }
+         return redirect('/user/project.list');
+     }
 
-    public function list(){
-        $project = Project::all();
-        //return view('project-list',compact('projects'));
-    }
+    // public function admin_project_list(){
+    //     $project = Project::all();
+    //     return view('admin.project_list');
+    // }
 
-    public function edit(Project $projects){
-        return view('projects.edit',compact('projects'));
-    }
+    // public function edit(Project $projects){
+    //     return view('projects.edit',compact('projects'));
+    // }
 
-    public function update(Request $request, Project $project)
-    {
-        $request->validate([
-            'issue_id' =>'required',
-            'project_name' => 'required|string|max:255',
-            'organization_name' => 'required',
-            'project_type' => 'required|string|max:255',
-            'project_manager' => 'required|string|max:255',
-            'contact_name' => 'nullable|string|max:255',
-            'contact_phone' => 'required|string|max:255',
-            'created_by' => 'required|string|max:255',
-            'updated_by' => 'required',
-            'deleted_by' => 'required',
-        ]);
+    // public function update(Request $request, $id)
+    // {
+    //     $request->validate([
+    //         'issue_id' =>'required',
+    //         'project_name' => 'required|string|max:255',
+    //         'organization_name' => 'required',
+    //         'project_type' => 'required|string|max:255',
+    //         'project_manager' => 'required|string|max:255',
+    //         'contact_name' => 'nullable|string|max:255',
+    //         'contact_phone' => 'required|string|max:255',
+    //         'created_by' => 'required',
+    //         'updated_by' => 'required',
+    //         'deleted_by' => 'required',
+    //     ]);
 
-        $project->update([
-            'id' => $request->input('id'),
-            'issue_id' => $request->input('issue_id'),
-            'project_name' => $request->input('project_name'),
-            'organization_name' => $request->input('organization_name'),
-            'project_type' => $request->input('project_type'),
-            'project_manager' => $request->input('project_manager'),
-            'contact_name' => $request->input('contact_name'),
-            'contact_phone' => $request->input('assignor_user'),
-            'contact_email' => $request->input('remark'),
-            'created_by' => $request->input('created_by'),
-            'updated_by' => $request->created_at,
-            'deleted_by' => $request->updated_at,
-        ]);
-    }
+    //     $project->update([
+    //         'id' => $request->input('id'),
+    //         'issue_id' => $request->input('issue_id'),
+    //         'project_name' => $request->input('project_name'),
+    //         'organization_name' => $request->input('organization_name'),
+    //         'project_type' => $request->input('project_type'),
+    //         'project_manager' => $request->input('project_manager'),
+    //         'contact_name' => $request->input('contact_name'),
+    //         'contact_phone' => $request->input('assignor_user'),
+    //         'contact_email' => $request->input('remark'),
+    //         'created_by' => $request->created_at,
+    //         'updated_by' => $request->created_at,
+    //         'deleted_by' => $request->updated_at,
+    //     ]);
+    // }
 
     public function delete(Project $project){
         $project->delete();
         return redirect('/user/project-list');
     }
+
 }
--- resources/views/user/project-list.blade.php
+++ resources/views/user/project-list.blade.php
@@ -1,35 +1,38 @@
 <x-layout>
-    <h1 class="text-xl text-blue-500 font-bold my-4 text-left ml-4">Project Lists</h1>
-    <!-- Table Horizontal and Vertical Scroll -->
-    <div class="overflow-x-auto overflow-y-auto max-w-full px-4 mb-8 rounded-md scrollbar-thin scrollbar-thumb-soft-purple scrollbar-track-gray-200">
+    <h1 class="text-xl font-bold my-4 text-left ml-4">Project Lists</h1>
+        <hr class="mb-6">
+ 
+        <!-- Table Horizontal and Vertical Scroll -->
+        <div class="overflow-x-auto overflow-y-auto max-w-full px-4 mb-8 rounded-md scrollbar-thin scrollbar-thumb-soft-purple scrollbar-track-gray-200">
         <table class="table-auto border-collapse border border-gray-300 min-w-[1500px] text-left">
-            <thead>
-                <tr class="bg-white text-blue">
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Project id</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Project Name</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Project Type</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Organization Name</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Contact Name</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Contact Email</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Contact Phone</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Created Date</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Status</th>
-                    <th class="border border-gray-300 px-19 py-3 text-lg truncate">Action</th>
-                </tr>
-            </thead>
-            <tbody>
+                <thead>
+                    <tr class="bg-white font-normal text-xs text-blue-800">
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Project-id</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Project Type</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Project Name</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Organization Name</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Contact Name</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Contact Email</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Contact Phone</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Created Date</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Status</th>
+                        <th class="border border-gray-300 px-19 py-3 text-lg truncate">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
                 @foreach ($projects as $project)
-                <tr class="hover:bg-gray-100">
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->id}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->project_name}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->project_type}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->organization_name}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_name}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_phone}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_email}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->created_at}}</td>
-                    <td class="border border-gray-300 px-8 py-3 text-xl text-green-500">Open</td>
-                    <td class="flex justify-between items-center">
+                    <!--Row 1-->
+                    <tr class="hover:bg-gray-100">
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->id}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->project_name}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->project_type}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->organization_name}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_name}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_email}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_phone}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->created_at}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl text-green-500">Active</td>
+                        <td class="flex justify-between items-center">
                         <form action="/project/{{$project->project_name}}/delete" method="POST">
                             @csrf
                             @method('DELETE')
@@ -39,15 +42,44 @@
                         </form>
                         <button class="bg-yellow-400 px-4 py-2 text-center hover:bg-yellow-600 hover:text-white">Update</button>
                     </td>
-                </tr>
-                @endforeach
-        
-            </tbody>
-        </table>
-    </div>
+                    </tr>
 
-    
-    <div class="my-4 flex justify-center">
+                    <!--Row 2-->
+                    <tr class="hover:bg-gray-100">
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->id}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->project_name}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->project_type}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->organization_name}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_name}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_email}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->contact_phone}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl">{{$project->created_at}}</td>
+                        <td class="border border-gray-300 px-8 py-3 text-xl text-red-500">Inctive</td>
+                        <td class="flex justify-between items-center">
+                        <form action="/project/{{$project->project_name}}/delete" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button
+                            type="submit"
+                            class="bg-red-400 px-4 py-2 mx-2 text-black hover:bg-red-600 hover:text-white">Delete</button>
+                        </form>
+                        <button class="bg-yellow-400 px-4 py-2 text-center hover:bg-yellow-600 hover:text-white">Update</button>
+                    </td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+
+        <a href="/user/new_project">
+    <button
+      class="flex items-center justify-start bg-purple-500 px-6 py-3 rounded-md hover:bg-purple-500 font-medium text-sm mx-2">
+      Add New
+    </button>
+  </a>
+
+  <div class="my-4 flex justify-center">
         <div class="bg-white shadow-md rounded-lg px-6 py-4">
             <ul class="space-x-2">
                 {{ $projects->links() }}
@@ -80,4 +112,5 @@
 
 
     </div> --}}
+
 </x-layout>
